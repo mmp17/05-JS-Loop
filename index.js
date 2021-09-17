b5d7d33d8a5b4716e04fe09397853535a87353ae
mmp17 <mirzaevmuzaffar@yahoo.com>
2021-09-17 10:44:54 +0900

    05 JS Loops

---
+++ index.js
@@ -0,0 +1,25 @@
+//05 JS Loop
+// step = (i,name = 'September') => {
+//     console.log(name, i+', 2021');
+// };
+
+// for (let i=1; i<=30; i++){
+//     step(i)
+// }
+
+// returns:
+// September 1, 2021
+// ..
+// September 30, 2021
+
+// for i in range(1,10):
+//   for j in range(1,11):
+//     print(i,'*',j,'=',i*j,' ')
+//   console.log('\n')
+
+// Creating pifagor table:
+for (let i=1; i<=10; i++)  
+for (let j=1; j<=10; j++) {
+     console.log(i,'*',j,'=',i*j)        
+    }
+    
